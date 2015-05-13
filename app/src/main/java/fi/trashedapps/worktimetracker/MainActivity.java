936ee0bce13dcfac580ac3eecf7ac6029cce5266
Codetrasher <jkortelahti@gmail.com>
2015-05-13 22:11:14 +0300

    Made some minor changes to the project.

--- app/src/main/java/fi/trashedapps/worktimetracker/MainActivity.java
+++ app/src/main/java/fi/trashedapps/worktimetracker/MainActivity.java
@@ -1,6 +1,5 @@
 package fi.trashedapps.worktimetracker;
 
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
