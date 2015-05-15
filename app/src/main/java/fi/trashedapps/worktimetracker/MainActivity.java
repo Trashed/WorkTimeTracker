4e150ccd50370ba2f79e6ace840c58f08c72ab4d
Codetrasher <jkortelahti@gmail.com>
2015-05-15 22:39:50 +0300

    Created new class, FileHandler. Added a static method it for creating a template CSV file.

--- app/src/main/java/fi/trashedapps/worktimetracker/MainActivity.java
+++ app/src/main/java/fi/trashedapps/worktimetracker/MainActivity.java
@@ -1,17 +1,36 @@
 package fi.trashedapps.worktimetracker;
 
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Widgets
+    private Button startStopButton;
+    // TODO: Remember to add holder variables for those two TextViews showing the time.
+    private TextView workStartTime;
+    private TextView workTimeLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Makes necessary init steps for the app
+        initializeApplication();
+
+        startStopButton = (Button)findViewById(R.id.button_start_stop_timer);
+        startStopButton.setTag("start");
+        startStopButton.setOnClickListener(this);
     }
 
     @Override
@@ -34,5 +53,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final String status = (String)v.getTag();
+        if(status.equals("start")) {
+            startStopButton.setText(R.string.start_button_stop_string);
+            startStopButton.setBackgroundResource(R.drawable.round_button_red);
+            startStopButton.setTag("stop");
+        }
+        else {
+            startStopButton.setText(R.string.start_button_start_string);
+            startStopButton.setBackgroundResource(R.drawable.round_button_green);
+            startStopButton.setTag("start");
+        }
+    }
+
+
+    private void initializeApplication() {
+        // Handle app's first launch. If true, write that value to false.
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        boolean isFirstLaunch = prefs.getBoolean("isFirstLaunch", true);
+        if(isFirstLaunch) {
+            SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+            editor.putBoolean("isFirstLaunch", false);
+            editor.apply();
+
+            // TODO: FileHandler - create a template CVS file for storing work time data from now on
+            try {
+                FileHandler.createTemplateCSV(getApplicationContext());
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("INIT ERROR", e.getMessage());
+            }
+        }
     }
 }
