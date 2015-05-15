6f59ce00597f95ac207ffec107419daebf5a46bc
Codetrasher <jkortelahti@gmail.com>
2015-05-15 23:07:11 +0300

    WorkTimeTracker: Added some methods for retrieving times. Needs implementation.

--- app/src/main/java/fi/trashedapps/worktimetracker/WorkTimeController.java
+++ app/src/main/java/fi/trashedapps/worktimetracker/WorkTimeController.java
@@ -14,4 +14,22 @@ public class WorkTimeController {
     public WorkTimeController(Context appContext) {
         // TODO: Kindly do the needed
     }
+
+    /**
+     * getWorkStartTime
+     * @return
+     */
+    public String getWorkStartTime() {
+        // TODO: Create a static variable or store the work start time value into SharedPrefs.
+        return null;
+    }
+
+    /**
+     * getWorkEndTime
+     * @return
+     */
+    public String getWorkEndTime() {
+        // TODO: Create a static variable or store the work end time value into SharedPrefs.
+        return null;
+    }
 }
