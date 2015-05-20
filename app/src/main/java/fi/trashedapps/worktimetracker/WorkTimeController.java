201025b085f66b1016ca774c6e652ce08868a76e
Codetrasher <jkortelahti@gmail.com>
2015-05-20 23:49:13 +0300

    Made several changes: controller returns start time; calculation of remaining working hours still WIP.

--- app/src/main/java/fi/trashedapps/worktimetracker/WorkTimeController.java
+++ app/src/main/java/fi/trashedapps/worktimetracker/WorkTimeController.java
@@ -3,6 +3,7 @@ package fi.trashedapps.worktimetracker;
 import android.content.Context;
 
 import java.sql.Time;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -13,12 +14,10 @@ import java.util.Locale;
  */
 public class WorkTimeController {
 
-    // Member variables
-    private Date startTimeAsDate;
-    private Date endTimeAsDate;
     private Calendar calendar;
     private String startTime;
     private String endTime;
+    private SimpleDateFormat sdf;
 
     /**
      * Constructor
@@ -26,6 +25,8 @@ public class WorkTimeController {
      */
     public WorkTimeController(Context appContext) {
         // TODO: Add stuff here if needed
+        calendar = Calendar.getInstance();
+        sdf = new SimpleDateFormat("HH:mm");
     }
 
     // TODO: Create a method that checks if user tries to log multiple hours within the same day.
@@ -33,36 +34,68 @@ public class WorkTimeController {
     /**
      * startWorkDay
      *
-     * Gets current time (HH:mm) and stores it
+     * Gets current time from Calendar, formats into HH:mm and stores it in a variable as well as
+     * saves the time value (HH:mm) into a file.
      */
     public void startWorkDay() {
-        // TODO: Needs implementation
-        startTimeAsDate = calendar.getTime();
-        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm", Locale.getDefault());
+        Date startTimeAsDate = calendar.getTime();
+        //SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        startTime = sdf.format(startTimeAsDate);
     }
 
     /**
      * endWorkDay
      */
     public void endWorkDay() {
-        // TODO: Needs implementation
+        Date endTimeAsDate = calendar.getTime();
+
+        endTime = sdf.format(endTimeAsDate);
     }
 
     /**
      * getWorkStartTime
-     * @return
+     * @return String startTime
      */
     public String getWorkStartTime() {
-        // TODO: Create a static variable or store the work start time value into SharedPrefs.
-        return null;
+        return startTime;
     }
 
     /**
      * getWorkEndTime
      * @return
      */
+    // TODO: Is this method needed?
     public String getWorkEndTime() {
         // TODO: Create a static variable or store the work end time value into SharedPrefs.
         return null;
+    }
+
+    /**
+     * calculateWorkingHoursLeft
+     *
+     * Calculates the time difference between start time and estimated end time and returns the
+     * difference as a string.
+     *
+     * @return String retVal
+     * @throws ParseException
+     */
+    public String calculateWorkingHoursLeft() throws ParseException {
+        Date start = sdf.parse(startTime);
+        // This value is 8 hrs because it includes half an hour lunch break.
+        Date fullWorkDay = sdf.parse("08:00");
+        Date end = sdf.parse(convertSecondsToHMmSs(start.getTime() + fullWorkDay.getTime()) + sdf.format(calendar.getTime()));
+
+        return sdf.format(end);
+    }
+
+
+    private static String convertSecondsToHMmSs(long seconds) {
+        long m = (seconds / 60) % 60;
+        long h = (seconds / (60 * 60)) % 24;
+        return String.format("%d:%02d", h, m);
+    }
+
+    private static long convertHoursToMs() {
+        return 0L;
     }
 }
